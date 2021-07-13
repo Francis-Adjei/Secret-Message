c9cd3a2ae5779c9b63e76f31bd04232041afa7db
Francis-Adjei <francis.adjei@amalitech.org>
2021-07-13 10:45:28 +0000

    Secret Message

---
+++ script.js
@@ -0,0 +1,36 @@
+ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+// console.log(secretMessage.length);
+secretMessage.pop();
+// console.log(secretMessage);
+
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program.');
+// console.log(secretMessage);
+
+console.log(secretMessage.indexOf('easily'));
+secretMessage[7] = 'right';
+// console.log(secretMessage);
+// console.log(secretMessage.indexOf('right'));
+
+secretMessage.shift();
+//  console.log(secretMessage);
+
+secretMessage.unshift('Programming');
+// console.log(secretMessage);
+
+
+secretMessage.splice(6,4);
+// console.log(secretMessage);
+secretMessage[6] = 'know';
+// console.log(secretMessage);
+
+console.log(secretMessage.join(" "));
+
+
+
+
+
+
+ 
+  
